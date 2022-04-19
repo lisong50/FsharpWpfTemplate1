e1e32af680cc7e8c70c027e31e6039cfd1679132
User <User@DESKTOP-BN45NQ4>
2022-04-20 08:48:32 +0900

    초기 커밋.

---
+++ FsharpWpfTemplate/MainWindow.xaml.fs
@@ -0,0 +1,9 @@
+﻿namespace FsharpWpfTemplate
+
+open System.Windows
+open FsharpWpfTemplate.ViewModels
+
+module MainWindow =
+  let initialize (window: Window) =
+    window.DataContext <- MainWindowViewModel()
+    ()
